7841773f6301affc6b96a83c9c9f4722a3a026d0
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-12-04 07:08:45 +0000

    [Fix]re4 コントローラー名修正

--- app/helpers/ralationships_helper.rb
+++
@@ -1,2 +0,0 @@
-module RalationshipsHelper
-end
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
   
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-    get 'followings' => 'ralationships#followings', as: 'followings'
-    get 'followers' => 'ralationships#followers', as: 'followers'
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
